9e3b6a1a5119a4c9e8fc2f8c3ed07e82f37aafa3
Normen Müller <normen.mueller@gmail.com>
2022-07-07 16:31:22 +0200

    Add MaybeT

--- monad-transformers/src/Lib.hs
+++ monad-transformers/src/Lib.hs
@@ -1,1 +1,20 @@
-module Lib where
+module Lib
+    ( MaybeT
+    , runMaybeT
+    ) where
+
+newtype MaybeT m a =
+    MaybeT
+        { runMaybeT :: m (Maybe a)
+        }
+
+instance (Functor m) => Functor (MaybeT m) where
+    fmap f = MaybeT . (fmap . fmap $ f) . runMaybeT
+
+instance (Functor m, Monad m) => Applicative (MaybeT m) where
+    pure = MaybeT . return . return
+    f <*> fa =
+        MaybeT $ do
+            f' <- runMaybeT f
+            a' <- runMaybeT fa
+            return $ f' <*> a'
