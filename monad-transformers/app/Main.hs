6e129beb9baeb4e8101bfe000a0d86602b6f2cad
Normen Müller <normen.mueller@gmail.com>
2022-08-26 13:28:28 +0200

    Add module 'Stack'

--- monad-transformers/app/Main.hs
+++ monad-transformers/app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Crafted
+import Stack
 
 main :: IO ()
 main = do
@@ -8,4 +9,7 @@ main = do
     let withInitialState = 5
     result <- Crafted.runApp app withInitialState
     print result
+    print "-- Stack"
+    print $ simulateStack [1, 2, 3]
+    print $ simulateStack' [1, 2, 3]
 
